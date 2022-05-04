79ebd6fa1320e2e582ea98e528d7c5eeebe20823
Maruf <maruf@terra.money>
2022-05-04 16:38:15 -0500

    done

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.get('/retrieve_files', (req, res) => {
             }
 
             return ({
-                "fileId": "",
+                "fileId": f_info.name,
                 "fileName": f_info.name,
                 "status": status,
                 "diceOutput": dice_score,
